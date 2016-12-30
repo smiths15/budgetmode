b09455956e35bc0b2205f2829c8b9ac24e7c9493
Smith Saintil <smith.saintil@gmail.com>
2016-12-30 22:47:14 +0000

    added lesson route on form for new lesson and added lesson titles on instructor scourse show page

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,14 @@ Rails.application.routes.draw do
   resources :courses, only: [:index, :show]
 
   namespace :instructor do
+    resources :sections, only: [] do 
+      resources :lessons, only: [:new, :create]
+    end
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create] do 
-        resources :lessons, only: [:new, :create]
-      end
+      resources :sections, only: [:new, :create]        
     end 
+ 
   end 
-  resources :users, only: :show
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
