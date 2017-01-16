5d25ac55f826a3c31769bb9467887cca5b190209
Smith Saintil <smith.saintil@gmail.com>
2017-01-16 12:35:24 +0000

    added dashboard and styled dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  resource :dashboard, only: [:show]
   root 'static_pages#index'
 
   resources :courses, only: [:index, :show] do
@@ -10,8 +11,7 @@ Rails.application.routes.draw do
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [:update]
-    resources :sections, only: [] do 
+    resources :sections, only: [:update] do 
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
