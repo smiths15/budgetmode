ab0397477d01835237779d77c558392a5ca7430c
Smith Saintil <smith.saintil@gmail.com>
2017-01-16 23:06:32 +0000

    add previous btn within lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,4 +4,26 @@ mount_uploader :video, VideoUploader
 
 include RankedModel
 ranks :row_order, :with_same => :section_id
+
+def next_lesson
+  lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+  
+  if lesson.blank? && section.next_section
+    return section.next_section.lessons.rank(:row_order).first
+  end 
+  
+   return lesson
+end
+
+def previous_lesson
+  prev_lesson = section.lessons.where("row_order < ?", self.row_order).rank(:row_order).last
+  
+  if prev_lesson.blank? && section.prev_section
+    return section.prev_section.lessons.rank(:row_order).last
+  end
+
+  return prev_lesson
+end
+
+
 end
